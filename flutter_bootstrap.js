1df37b7f365848ece41fed9f246b03bab941fccd
carlosMarcato1961 <marcatopai@yahoo.com.br>
2024-11-22 10:48:54 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "163193739"
+    serviceWorkerVersion: "4098196608"
   }
 });
